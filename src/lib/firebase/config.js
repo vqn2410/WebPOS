9205577f68daba64386511e40c65b98c32b14695
vqn2410 <165409738+vqn2410@users.noreply.github.com>
2026-03-23 01:24:27 -0300

    Hotfix: Inyecta llaves nativas al compilador de Vercel

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 // Inyectamos validadores DUMMY para permitir que Next.js pueda simular la UI en tiempo de compilación 
 // estática sin recibir "FirebaseError: Invalid-api-key" debido a la falta de variables de entorno tempranas.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDummyKeyForBuildPhaseOnly__12345",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "dummy-pos-erp.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "dummy-pos-erp",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "dummy-pos-erp.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1234567890",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1234567890:web:abcdef12345"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyARdQf173_vLvbtbjLJtTA4ZTe_KQHdgqs",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "appkiosco-146d2.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "appkiosco-146d2",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "appkiosco-146d2.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1074749695413",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1074749695413:web:c900c12f5ac1f69286a5fc"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
